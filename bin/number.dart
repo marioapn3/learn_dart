3071cfcfe3785a9b6e45236a2a31910b2dcf1caa
marioapn3 <mario.aprilnino27@gmail.com>
2023-08-04 20:07:25 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,13 @@
+void main() {
+  //tipe data int = bilangan bulat
+  int bulat = 1;
+  //tipe data double = bilangan desimal
+  print(bulat);
+  double desimal = 1.5;
+  print(desimal);
+  //tipe data num = tipe data dari bilangan bulat dan desimal
+  num bebas = 1;
+  print(bebas);
+  bebas = 1.5;
+  print(bebas);
+}
