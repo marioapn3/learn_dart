200c3d0ef3223563fbda52fbe94ac68381b873aa
marioapn3 <mario.aprilnino27@gmail.com>
2023-08-04 19:50:32 +0700

    hello world

--- bin/learn_dart.dart
+++ bin/learn_dart.dart
@@ -1,5 +1,9 @@
 import 'package:learn_dart/learn_dart.dart' as learn_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${learn_dart.calculate()}!');
+// void main(List<String> arguments) {
+//   print('Hello world: ${learn_dart.calculate()}!');
+// }
+
+void main() {
+  print('Hello World');
 }
